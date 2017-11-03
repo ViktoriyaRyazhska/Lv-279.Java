615dab553f061ee0ddbc7e2a0a3d6749991ec0a6
orok7 <orestmykytyn@gmail.com>
2017-11-03 16:56:50 +0200

    init my branch ToDoDA

---
+++ LinkedList/src/main/java/ua/softserve/academy/linkedlist/dynamic/array/ToDoListDynamicArray.java
@@ -0,0 +1,17 @@
+/*
+* ToDoListDynamicArray class for making To-Do list using  Dynamic Array
+*
+* Version 1.0-SNAPSHOT
+*
+* 03.11.17
+*
+* All rights reserved by DoubleO Team (Team#1)
+* */
+
+package ua.softserve.academy.linkedlist.dynamic.array;
+
+public class ToDoListDynamicArray {
+
+
+
+}
