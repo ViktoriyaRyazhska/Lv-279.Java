c03ac1d70e4fed121ad00426482e0709e5b6289b
olegfuks <olegfuks98@gmail.com>
2017-11-05 16:04:17 +0200

    tests

--- LinkedList/src/test/java/ua/softserve/academy/linkedlist/circular/linked/list/CircularLinkedListTest.java
+++ LinkedList/src/test/java/ua/softserve/academy/linkedlist/circular/linked/list/CircularLinkedListTest.java
@@ -9,6 +9,7 @@
 * */
 package ua.softserve.academy.linkedlist.circular.linked.list;
 
+import org.junit.Before;
 import org.junit.Test;
 import ua.softserve.academy.linkedlist.circular.linked.list.interfaces.ICircularLinkedList;
 
@@ -17,60 +18,56 @@ import java.util.Iterator;
 import static org.junit.Assert.*;
 
 public class CircularLinkedListTest {
+    ICircularLinkedList<String> list;
 
-    @Test
-    public void iterator() throws Exception {
-
+    @Before
+    public void init(){
+        list =new CircularLinkedList<>();
     }
 
     @Test
     public void add() throws Exception {
-        ICircularLinkedList<String> list = new CircularLinkedList<>();
         list.add("a");
-        list.add("c");
-        list.add("b");
-        System.out.println(list.size());
-        System.out.println(list);
+        assertEquals(true,list.contains("a"));
     }
 
     @Test
     public void remove() throws Exception {
-        ICircularLinkedList<String> list = new CircularLinkedList<>();
         list.add("a");
-        list.add("c");
         list.add("b");
-        System.out.println(list.size());
-        System.out.println(list);
-        System.out.println(list.remove("c"));
-        System.out.println(list.size());
-        System.out.println(list);
+        list.add("b");
+        list.remove("a");
+        assertEquals(false, list.contains("a"));
     }
 
     @Test
     public void contains() throws Exception {
-        ICircularLinkedList<String> list = new CircularLinkedList<>();
         list.add("a");
         list.add("c");
         list.add("b");
-        System.out.println(list.size());
-        System.out.println(list.contains("c"));
+        assertEquals(true, list.contains("a"));
+        assertEquals(true, list.contains("b"));
+        assertEquals(true, list.contains("c"));
+        assertEquals(false, list.contains("cdas"));
     }
 
     @Test
     public void size() throws Exception {
+        list.add("a");
+        list.add("c");
+        list.add("b");
+        assertEquals(3, list.size());
     }
 
     @Test
     public void clear() throws Exception {
-        ICircularLinkedList<String> list = new CircularLinkedList<>();
         list.add("a");
         list.add("c");
         list.add("b");
-        System.out.println(list.size());
-        System.out.println(list);
         list.clear();
-        System.out.println(list.size());
         System.out.println(list);
+        assertEquals(0, list.size());
+        assertEquals(false, list.contains(null));
     }
 
 }
