2e2f4ee7e0c0dfd7aef9977eebc1e465378543d0
Orest Melnyk <o.melnyk10@gmail.com>
2017-11-05 11:20:06 +0200

    add tests 4 LL

---
+++ LinkedList/src/test/java/ua/softserve/academy/linkedlist/linked/list/impl/LinkedListTest.java
@@ -0,0 +1,84 @@
+package ua.softserve.academy.linkedlist.linked.list.impl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class LinkedListTest {
+    LinkedList<String> stringLinkedList;
+
+    @Before
+    public void setUp() throws Exception {
+        stringLinkedList = new LinkedList<>();
+        stringLinkedList.add("Tom");
+        stringLinkedList.add("Bob");
+        stringLinkedList.add("Jack");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        stringLinkedList = null;
+    }
+
+    @Test
+    public void get() throws Exception {
+        assertEquals("Bob", stringLinkedList.get(1));
+        assertEquals("Tom", stringLinkedList.get(0));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getNegative() throws Exception {
+        stringLinkedList.get(-1);
+    }
+
+    @Test
+    public void size() throws Exception {
+        assertEquals(3, stringLinkedList.size());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void nullSize() throws Exception {
+        stringLinkedList = null;
+        stringLinkedList.size();
+    }
+
+    @Test
+    public void contains() throws Exception {
+        assertEquals(true, stringLinkedList.contains("Tom"));
+        assertEquals(false, stringLinkedList.contains(null));
+
+        stringLinkedList.add("Steve");
+        assertEquals(true, stringLinkedList.contains("Steve"));
+    }
+
+    @Test
+    public void remove() throws Exception {
+        stringLinkedList.remove("Bob");
+        assertEquals(2, stringLinkedList.size());
+        assertEquals("Tom", stringLinkedList.get(0));
+        assertEquals("Jack", stringLinkedList.get(1));
+    }
+
+    @Test
+    public void clear() throws Exception {
+        stringLinkedList.clear();
+        assertEquals(0, stringLinkedList.size());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getAfterClear() throws Exception {
+        stringLinkedList.clear();
+        stringLinkedList.get(0);
+    }
+
+    @Test
+    public void add() throws Exception {
+        stringLinkedList.add("Steve");
+        assertEquals("Steve", stringLinkedList.get(3));
+    }
+
+
+}
