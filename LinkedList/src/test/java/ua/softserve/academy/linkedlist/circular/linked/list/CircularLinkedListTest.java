5c00d4ce188a5cad1e3209bb87c8ca559b762039
grosol <rosgrynyk@gmail.com>
2017-11-06 12:55:13 +0200

    buffer

--- LinkedList/src/test/java/ua/softserve/academy/linkedlist/circular/linked/list/CircularLinkedListTest.java
+++ LinkedList/src/test/java/ua/softserve/academy/linkedlist/circular/linked/list/CircularLinkedListTest.java
@@ -1,20 +1,19 @@
-/*
-* CircularLinkedListTest
+/* CircularLinkedListTest
 *
 * Version 1.0-SNAPSHOT
 *
 * 04.11.17
 *
 * All rights reserved by DoubleO Team (Team#1)
-* */
+*/
+
 package ua.softserve.academy.linkedlist.circular.linked.list;
 
 import org.junit.Before;
 import org.junit.Test;
 import ua.softserve.academy.linkedlist.circular.linked.list.interfaces.ICircularLinkedList;
 
-import java.ua.softserve.academy.linkedlist.circular.linked.list.dobleo.CircularLinkedList;
-import java.util.Iterator;
+
 
 import static org.junit.Assert.*;
 
