15cf9c5e8605c2199858e9e774a80fb5a5d7804e
orok7 <orestmykytyn@gmail.com>
2017-11-06 15:44:40 +0200

    javadoc for CircularLinkedList was added

--- LinkedList/src/main/java/ua/softserve/academy/linkedlist/circular/linked/list/dobleo/JosephusProblem.java
+++ LinkedList/src/main/java/ua/softserve/academy/linkedlist/circular/linked/list/dobleo/JosephusProblem.java
@@ -14,27 +14,31 @@ import ua.softserve.academy.linkedlist.circular.linked.list.dobleo.interfaces.IC
 
 import java.util.Iterator;
 
+/**
+ * Class for solving Josephus problem using circular linked list.
+ */
 public class JosephusProblem {
 
+    /**
+     * Invoke this method for solving Josephus problem using circular linked list.
+     * @param size amount of elements.
+     * @return result.
+     */
     public static int execute(int size) {
-
-        if (size < 1) return 0;
-
+        if (size < 1) {
+            return 0;
+        }
         ICircularLinkedList<Integer> list = new CircularLinkedList<>();
-
         for (int i = 1; i <= size; i++){
             list.add(i);
         }
-
         Iterator<Integer> iterator = list.iterator();
         iterator.hasNext();
         while (list.size() > 1){
             iterator.hasNext();
             iterator.remove();
         }
-
         return iterator.next();
-
     }
 
 }
