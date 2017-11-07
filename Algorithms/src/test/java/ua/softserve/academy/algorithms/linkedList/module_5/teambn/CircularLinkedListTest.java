6083d72c3f3d379aab36e71358b3f769a9f430d5
Nazarusko <nazar.davidovskij@gmail.com>
2017-11-07 21:45:20 +0200

    Add Module 5

---
+++ Algorithms/src/test/java/ua/softserve/academy/algorithms/linkedList/module_5/teambn/CircularLinkedListTest.java
@@ -0,0 +1,91 @@
+/*
+* CircularLinkedListTest
+*
+* Version 1.0-SNAPSHOT
+*
+* 05.11.17
+*
+* All rights reserved by TeamBN (Team#2)
+* */
+package ua.softserve.academy.algorithms.linkedList.module_5.teambn;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ua.softserve.academy.algorithms.linkedList.module_5.teambn.interfaces.ICircularLinkedList;
+
+import static org.junit.Assert.assertEquals;
+
+public class CircularLinkedListTest {
+    ICircularLinkedList<String> testList;
+
+    @Before
+    public void setUp() throws Exception {
+        testList = new CircularLinkedList<>();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        testList = null;
+    }
+
+    @Test
+    public void add() throws Exception {
+        testList.add("Nazar");
+        testList.add("Bohdan");
+        assertEquals(true, testList.contains("Nazar"));
+        assertEquals(false, testList.contains("Naz"));
+        testList.clear();
+    }
+
+    @Test
+    public void remove() throws Exception {
+        testList.add("Nazar");
+        testList.add("Bohdan");
+        testList.remove("Nazar");
+        assertEquals(false, testList.contains("Nazar"));
+        assertEquals(true, testList.contains("Bohdan"));
+        testList.add("Oleh");
+        testList.remove("Oleh");
+        assertEquals(false, testList.contains("Oleh"));
+        testList.clear();
+    }
+
+    @Test
+    public void next() throws Exception {
+        testList.add("Nazar");
+        testList.add("Bohdan");
+        String expected = testList.next();
+        assertEquals(expected, "Nazar");
+        testList.clear();
+    }
+
+    @Test
+    public void contains() throws Exception {
+        testList.add("Nazar");
+        testList.remove("Nazar");
+        assertEquals(false, testList.contains("Nazar"));
+        testList.add("Bohdan");
+        assertEquals(true, testList.contains("Bohdan"));
+        testList.add("Oleh");
+        testList.remove("Oleh");
+        assertEquals(false, testList.contains("Oleh"));
+        testList.clear();
+    }
+
+    @Test
+    public void size() throws Exception {
+        testList.add("Nazar");
+        assertEquals(1, testList.size());
+        testList.remove("Nazar");
+        assertEquals(0, testList.size());
+        testList.clear();
+    }
+
+    @Test
+    public void clear() throws Exception {
+        testList.add("Nazar");
+        testList.clear();
+        assertEquals(0, testList.size());
+    }
+}
---
+++ Algorithms/src/test/java/ua/softserve/academy/algorithms/linkedList/module_5/teambn/JosephusProblemTest.java
@@ -0,0 +1,27 @@
+/*
+* JosephusProblemTest
+*
+* Version 1.0-SNAPSHOT
+*
+* 07.11.17
+*
+* All rights reserved by TeamBN (Team#2)
+* */
+package ua.softserve.academy.algorithms.linkedList.module_5.teambn;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class JosephusProblemTest {
+
+    @Test
+    public void killThemAll() throws Exception {
+
+        assertEquals(19, JosephusProblem.killThemAll(41));
+        assertEquals(7, JosephusProblem.killThemAll(7));
+        assertEquals(0, JosephusProblem.killThemAll(0));
+        assertEquals(0, JosephusProblem.killThemAll(-100));
+    }
+
+}
