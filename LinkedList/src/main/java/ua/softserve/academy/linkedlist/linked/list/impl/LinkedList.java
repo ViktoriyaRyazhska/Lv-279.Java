5198f0294632bd585cb43abba77db07d7780623b
AndriiBarna <andrybarna95@gmail.com>
2017-11-08 23:51:46 +0200

    Add some fixs to the method add value at position to LinkedList

--- LinkedList/src/main/java/ua/softserve/academy/linkedlist/linked/list/impl/LinkedList.java
+++ LinkedList/src/main/java/ua/softserve/academy/linkedlist/linked/list/impl/LinkedList.java
@@ -150,31 +150,28 @@ public class LinkedList<T> {
             size++;
         }
     }
+
     public void add(int index, T elem) {
 
         if (elem == null) {
             throw new IllegalArgumentException("Null argument");
-        }
-        if (!checkIndex(index)) throw new IndexOutOfBoundsException();
-
-        if (index < size / 2) {
+        } else if ((size == 0) && (index == 0)) {
+            add(elem);
+        } else if (!checkIndex(index)) {
+            throw new IndexOutOfBoundsException();
+        } else if (index < size / 2) {
 
             Node<T> el = first;
             for (int i = 0; i < index; i++)
                 el = el.getNext();
             repositioningForAdding(el, elem);
-
-
         } else {
             Node<T> el = last;
             for (int i = size - 1; i > index; i--)
                 el = el.getPrevious();
 
             repositioningForAdding(el, elem);
-
         }
-
-
     }
 
     private void repositioningForAdding(Node<T> el, T elem) {
@@ -239,7 +236,9 @@ public class LinkedList<T> {
             @Override
             public void insert(T value) {
 
-                if (this.getCurrent().getValue() == null) {throw new NoSuchElementException();}
+                if (this.getCurrent().getValue() == null) {
+                    throw new NoSuchElementException();
+                }
                 LinkedList.this.repositioningForAdding(this.getCurrent(), value);
             }
 
