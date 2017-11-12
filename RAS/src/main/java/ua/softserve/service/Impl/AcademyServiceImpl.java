8377a8f90236e698dfd118d260d937330a85de99
Orest Melnyk <o.melnyk10@gmail.com>
2017-11-12 17:47:17 +0200

    add realization

--- RAS/src/main/java/ua/softserve/service/Impl/AcademyServiceImpl.java
+++ RAS/src/main/java/ua/softserve/service/Impl/AcademyServiceImpl.java
@@ -1,4 +1,28 @@
 package ua.softserve.service.Impl;
 
-public class AcademyServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ua.softserve.data.entity.Academy;
+import ua.softserve.repository.dao.AcademyDAO;
+import ua.softserve.service.AcademyService;
+
+import java.util.List;
+
+@Service
+public class AcademyServiceImpl implements AcademyService {
+    @Autowired
+    AcademyDAO academyDAO;
+
+    @Transactional
+    @Override
+    public Academy getById(Integer id) {
+        return academyDAO.getById(id);
+    }
+
+    @Transactional
+    @Override
+    public List<Academy> getAllAcademys() {
+        return academyDAO.getAllAcademys();
+    }
 }
