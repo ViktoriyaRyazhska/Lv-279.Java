8a76ed91466b42491917ec209140c3dc3b3a6cc8
Orest Melnyk <o.melnyk10@gmail.com>
2017-11-17 22:12:23 +0200

    delete unnecessary imports

--- RAS/web/src/main/java/ua/softserve/config/WebInit.java
+++ RAS/web/src/main/java/ua/softserve/config/WebInit.java
@@ -1,13 +1,6 @@
 package ua.softserve.config;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 
 public class WebInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
