4128a589cd2de8e65276963ee60485ee24aff1a2
Orest Melnyk <o.melnyk10@gmail.com>
2017-11-20 18:32:07 +0200

    add method 4 get all city names

--- RAS/persistence/src/main/java/ua/softserve/persistence/dao/Language_TranslationsDAO.java
+++ RAS/persistence/src/main/java/ua/softserve/persistence/dao/Language_TranslationsDAO.java
@@ -1,7 +1,12 @@
 package ua.softserve.persistence.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ua.softserve.persistence.entity.Language_Translations;
 
+import java.util.List;
+
 public interface Language_TranslationsDAO extends JpaRepository<Language_Translations, Integer> {
+    @Query("select lt from Language_Translations lt where lt.tag like 'city' and local like 'en'")
+    List<Language_Translations> getAllLanguageTranslationsName();
 }
