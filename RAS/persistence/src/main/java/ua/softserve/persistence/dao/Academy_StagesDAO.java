e2c64198a830f16812ceb4bfc04ecbc3a0cf1ff6
Orest Melnyk <o.melnyk10@gmail.com>
2017-11-20 18:32:36 +0200

    add new method findAll

--- RAS/persistence/src/main/java/ua/softserve/persistence/dao/Academy_StagesDAO.java
+++ RAS/persistence/src/main/java/ua/softserve/persistence/dao/Academy_StagesDAO.java
@@ -1,11 +1,10 @@
 package ua.softserve.persistence.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ua.softserve.persistence.entity.Academy;
 import ua.softserve.persistence.entity.Academy_Stages;
 
 import java.util.List;
 
 public interface Academy_StagesDAO extends JpaRepository<Academy_Stages, Integer> {
-
+    List<Academy_Stages> findAll();
 }
