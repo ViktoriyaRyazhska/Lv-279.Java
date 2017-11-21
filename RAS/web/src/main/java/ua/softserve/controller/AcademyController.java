48675c9c68624a125830866c446c0d9284f96fef
Orest Melnyk <o.melnyk10@gmail.com>
2017-11-21 21:34:05 +0200

    refactor jsp and contoller

--- RAS/web/src/main/java/ua/softserve/controller/AcademyController.java
+++ RAS/web/src/main/java/ua/softserve/controller/AcademyController.java
@@ -63,14 +63,6 @@ public class AcademyController {
         List<Technologies> technologie = technologieService.findAll();
         List<Profile> profile = profileService.findAll();
 
-        Academy academy = academyService.getById(796);
-
-
-        //        model.addAttribute("student_group_count", academy.getStudent_group_count());
-        model.addAttribute("name", academy.getName());
-        model.addAttribute("startDate", academy.getStartDate());
-        model.addAttribute("endDate", academy.getEndDate());
-        model.addAttribute("free", academy.getFree());
         model.addAttribute("academyStages", academyStages);
         model.addAttribute("cityNames", cityNames);
         model.addAttribute("direction", direction);
@@ -80,12 +72,16 @@ public class AcademyController {
     }
 
 
-//    @RequestMapping(value = "/addGroup", method = RequestMethod.POST)
-//    public String submit(@Valid @ModelAttribute("academy") final Academy academy, final BindingResult result, final ModelMap model) {
+    @RequestMapping(value = "/addGroup", method = RequestMethod.POST)
+    public String submit(@Valid @ModelAttribute("academy") final Academy academy, final BindingResult result, final ModelMap model) {
 //        if (result.hasErrors()) {
 //            return "error";
 //        }
-//        model.addAttribute("cityNames", academy.getCity());
+
+        System.out.println(academy.getCity());
+        model.addAttribute("cityNames", academy.getCity());
+        System.out.println(academy.getCity());
+
 //        model.addAttribute("direction", academy.getDirections());
 //        model.addAttribute("technologie", academy.getTechnologies());
 //        model.addAttribute("academyStages", academy.getAcademy_stages());
@@ -95,10 +91,10 @@ public class AcademyController {
 //        model.addAttribute("startDate", academy.getStartDate());
 //        model.addAttribute("endDate", academy.getEndDate());
 //        model.addAttribute("free", academy.getFree());
-//
-//        academyService.save(academy);
-//        return "academy";
-//    }
+
+        academyService.save(academy);
+        return "academy";
+    }
 
 
 
