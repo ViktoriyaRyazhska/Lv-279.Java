81c047f36929a9120942b609f782f5b859dbb35a
Orest Melnyk <o.melnyk10@gmail.com>
2017-11-22 20:01:12 +0200

    add AcademyDTO

---
+++ RAS/service/src/main/java/ua/softserve/dto/AcademyDTO.java
@@ -0,0 +1,29 @@
+package ua.softserve.dto;
+
+import java.sql.Timestamp;
+
+import lombok.*;
+import ua.softserve.persistence.entity.*;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class AcademyDTO {
+
+    private String grName;
+    private String nameForSite;
+    private AcademyStages academyStages;
+    private String startDate;
+    private String endDate;
+    private String cityNames;
+    private int payment;
+    private Directions direction;
+    private Technologies technologie;
+    private Profile profile;
+//    private String planedToGraduate;
+//    private String planedToEnrollment;
+//    private String countActual;
+    private StudentGroupCount studentGroupCount;
+
+}
