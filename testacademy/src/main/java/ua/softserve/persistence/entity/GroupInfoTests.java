83d4a69a7ff69cdc0db17dc51697d28ffd14323d
orok7 <orestmykytyn@gmail.com>
2017-12-04 16:37:30 +0200

    entities with tests

--- testacademy/src/main/java/ua/softserve/persistence/entity/GroupInfoTests.java
+++ testacademy/src/main/java/ua/softserve/persistence/entity/GroupInfoTests.java
@@ -32,5 +32,64 @@ public class GroupInfoTests {
     @JoinColumn(name = "academy_id")
     private Academy academy;
 
+    @Column(name = "test_1_name")
+    private String testOneName;
+
+    @Column(name = "test_1_max_val")
+    private String testOneMaxVal;
+
+    @Column(name = "test_2_name")
+    private String testTwoName;
+
+    @Column(name = "test_2_max_val")
+    private String testTwoMaxVal;
+
+    @Column(name = "test_3_name")
+    private String testThreeName;
+
+    @Column(name = "test_3_max_val")
+    private String testThreeMaxVal;
+
+    @Column(name = "test_4_name")
+    private String testFourName;
+
+    @Column(name = "test_4_max_val")
+    private String testFourMaxVal;
+
+    @Column(name = "test_5_name")
+    private String testFiveName;
+
+    @Column(name = "test_5_max_val")
+    private String testFiveMaxVal;
+
+    @Column(name = "test_6_name")
+    private String testSixName;
+
+    @Column(name = "test_6_max_val")
+    private String testSixMaxVal;
+
+    @Column(name = "test_7_name")
+    private String testSevenName;
+
+    @Column(name = "test_7_max_val")
+    private String testSevenMaxVal;
+
+    @Column(name = "test_8_name")
+    private String testEightName;
+
+    @Column(name = "test_8_max_val")
+    private String testEightMaxVal;
+
+    @Column(name = "test_9_name")
+    private String testNineName;
+
+    @Column(name = "test_9_max_val")
+    private String testNineMaxVal;
+
+    @Column(name = "test_10_name")
+    private String testTenName;
+
+    @Column(name = "test_10_max_val")
+    private String testTenMaxVal;
 
 }
