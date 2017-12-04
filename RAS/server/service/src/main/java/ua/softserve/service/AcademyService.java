7f8ce213e4d9b2c20774fb5a9d4df24a89d058da
shevtsov.bohdan <shevtsov.bohdan@gmail.com>
2017-12-04 16:58:23 +0200

    edited AcademyDTO

--- RAS/server/service/src/main/java/ua/softserve/service/AcademyService.java
+++ RAS/server/service/src/main/java/ua/softserve/service/AcademyService.java
@@ -9,23 +9,13 @@ import java.util.List;
 public interface AcademyService {
     Academy getById(Integer id);
 
-    List<Academy> getAllAcademys();
-
-    List<Academy> findAllByName(String name);
-
     void save(Academy user);
-
-    void saveDTO(AcademyDTO academyDTO);
 
     void saveCustom(int id,String role,int[] arr, EmployeeService employeeService);
 
     Academy findOne(int id);
 
-    Academy findWithEmployeeTeacher(int id);
-
-    Academy findWithEmployeeExperts(int id);
-
-    Academy findWithEmployeeInterviewers(int id);
-
     List<AcademyDTO> findWithEmployeeExperts();
+
+    AcademyDTO getAcademyDTO();
 }
--- RAS/server/service/src/main/java/ua/softserve/service/DropBoxForAcademyDTOService.java
+++
@@ -1,7 +0,0 @@
-package ua.softserve.service;
-
-import ua.softserve.service.dto.DropBoxForAcademyDTO;
-
-public interface DropBoxForAcademyDTOService {
-    DropBoxForAcademyDTO getDropBox();
-}
