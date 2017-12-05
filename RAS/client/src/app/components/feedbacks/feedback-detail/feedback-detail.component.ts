88953cc2564b2f149bcc65965805c4d9408bcb09
Nazarusko <nazar.davidovskij@gmail.com>
2017-12-05 17:02:05 +0200

    Add new feature

---
+++ RAS/client/src/app/components/feedbacks/feedback-detail/feedback-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-detail',
+  templateUrl: './feedback-detail.component.html',
+  styleUrls: ['./feedback-detail.component.css']
+})
+export class FeedbackDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ RAS/client/src/app/components/feedbacks/feedback-list/feedback-item/feedback-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-item',
+  templateUrl: './feedback-item.component.html',
+  styleUrls: ['./feedback-item.component.css']
+})
+export class FeedbackItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
