707f3f7a16e53509a058f429d95211eacadf3bc5
“Vasia <vpupkin@mail.com>
2017-12-06 17:51:49 +0200

    routing man)

--- Dump/DumpVer3.sql
+++ Dump/DumpVer3.sql
@@ -3,7 +3,7 @@
 -- Host: localhost    Database: academy_test
 -- ------------------------------------------------------
 -- Server version	5.7.20-0ubuntu0.16.04.1
-
+use ss_ps_db;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
