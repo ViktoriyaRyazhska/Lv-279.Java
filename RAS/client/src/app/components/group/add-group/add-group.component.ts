9d973fa85e986eb0fdc0083632fe36ca98f22db7
Orest Melnyk <o.melnyk10@gmail.com>
2017-12-08 23:49:55 +0200

    refactor array names

--- RAS/client/src/app/components/group/add-group/add-group.component.ts
+++ RAS/client/src/app/components/group/add-group/add-group.component.ts
@@ -4,8 +4,6 @@ import {AddGroupService} from "./add-group.service";
 import {HistoryService} from "../../history/history.service";
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 
-
-
 @Component({
   selector: 'app-add-group',
   templateUrl: './add-group.component.html',
@@ -15,19 +13,16 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class AddGroupComponent implements OnInit {
   signupForm: FormGroup;
 
-  academyStages: any[];
-  cityNames: any[];
+  academyStatus: any[];
+  city: any[];
+  commonDirection: any[];
   direction: any[];
-  technologie: any[];
   profile: any[];
-
-  technologyModel: any;
 
   defaultStudentActual: number = 0;
   defaultStudentsPlannedToGraduate: number = 0;
   defaultStudentsPlannedToEnrollment: number = 0;
 
-  defaultCity: any[];
   defaultInvalidInput: string = 'No data entered. Group will not be save';
 
   constructor(private addGroupService: AddGroupService) {
@@ -35,11 +30,11 @@ export class AddGroupComponent implements OnInit {
 
   ngOnInit() {
     this.addGroupService.getAll().subscribe(resp => {
-      this.academyStages = resp.academyStages;
-      this.cityNames = resp.cityNames;
-      this.direction = resp.direction;
+      this.academyStatus = resp.academyStages;
+      this.city = resp.cityNames;
+      this.commonDirection = resp.direction;
+      this.direction = resp.technologie;
       this.profile = resp.profile;
-      this.technologie = resp.technologie;
     });
 
     this.signupForm = new FormGroup({
@@ -49,7 +44,7 @@ export class AddGroupComponent implements OnInit {
       'cityId': new FormControl('1'),
       'startDateFormControl': new FormControl(),
       'endDateFormControl': new FormControl(),
-      'commonDirection': new FormControl(),
+      'commonDirectionFormControl': new FormControl(),
       'technolgyId': new FormControl(),
       'profileInfoFormControl': new FormControl(),
       'SCTGraduate': new FormControl(),
