57a19bec7a16c9eb32e518a4bb5ff79ca1ee6fa1
grosol <rosgrynyk@gmail.com>
2017-12-08 09:13:48 +0200

    load 12/8/2017'

--- RAS/server/service/src/main/java/ua/softserve/service/converter/AcademyConverter.java
+++ RAS/server/service/src/main/java/ua/softserve/service/converter/AcademyConverter.java
@@ -32,9 +32,9 @@ public class AcademyConverter {
             academyDTO.setDirectionName(groupInfo.getAcademy().getDirections().getName());
             academyDTO.setTechnologyName(groupInfo.getAcademy().getTechnologies().getName());
             academyDTO.setPayment(groupInfo.getAcademy().getFree());
-            if(groupInfo.getAcademy().getFree() == 1){
+            if (groupInfo.getAcademy().getFree() == 1) {
                 academyDTO.setPaymentStatus("Founded by SoftServe academy");
-            }else{
+            } else {
                 academyDTO.setPaymentStatus("Paid");
             }
             academyDTO.setEndDate(groupInfo.getAcademy().getEndDate().toString());
