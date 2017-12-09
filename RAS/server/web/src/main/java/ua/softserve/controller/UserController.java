3db3fab14b966892b4990dea4fe9426307cd7384
orok7 <orestmykytyn@gmail.com>
2017-12-09 20:08:53 +0200

    check list by groups dto, service, controller, ui

--- RAS/server/web/src/main/java/ua/softserve/controller/UserController.java
+++ RAS/server/web/src/main/java/ua/softserve/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ua.softserve.persistence.entity.*;
+import ua.softserve.persistence.entity.User;
 import ua.softserve.service.UserService;
 
 import java.util.List;
