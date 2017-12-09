91990b014ba3470897af9e03cc71507c1bd997e9
Orest Melnyk <o.melnyk10@gmail.com>
2017-12-09 19:02:07 +0200

    add method for set data from form

--- RAS/client/src/app/components/group/add-group/group.model.ts
+++ RAS/client/src/app/components/group/add-group/group.model.ts
@@ -1,16 +1,34 @@
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+
 export class Group {
 
-  groupInfo: string;
-  nameForSite: string;
-  academyStatusId: number;
-  cityId: number;
-  startDate: string;
-  endDate: string;
-  commonDirectionId: number;
+  grName: string = null;
+  nameForSite: string = null;
+  academyStagesId: number = 1;
+  startDate: string = null;
+  endDate: string = null;
+  cityId: number = 1;
   directionId: number;
-  profileInfoId: number;
-  sctGraduate: number;
-  sctEnrollment: number;
-  studentActual: number;
+  technologieId: number;
+  profileId: number;
+  studentPlannedToGraduate: number = 0;
+  studentPlannedToEnrollment: number = 0;
+  studentActual: number = 0;
+
+  setDataFromFormControl(form: FormGroup){
+    this.grName = form.get('groupInfoFormControl').value;
+    this.nameForSite = form.get('nameForSiteFormControl').value;;
+    this.academyStagesId = form.get('academyStagesId').value;;
+    this.cityId = form.get('cityId').value;;
+    this.startDate = form.get('startDateFormControl').value;;
+    this.endDate = form.get('endDateFormControl').value;;
+    this.directionId = form.get('commonDirectionFormControl').value;;
+    this.technologieId = form.get('directionFormControl').value;;
+    this.profileId = form.get('profileInfoFormControl').value;;
+    this.studentPlannedToGraduate = form.get('studentPlannedToGraduate').value;
+    this.studentPlannedToEnrollment = form.get('studentPlannedToEnrollment').value;;
+    // this.studentActual = form.get('').value;;
+  }
 
 }
