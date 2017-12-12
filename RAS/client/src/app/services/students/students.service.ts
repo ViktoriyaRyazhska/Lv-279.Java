7985f27ed3e37aeafe9bb7908ae29f229b6bc723
AndriiBarna <andrybarna95@gmail.com>
2017-12-13 00:30:45 +0200

    Add methods for getting student statuses from data base; add inputs in datatable

--- RAS/client/src/app/services/students/students.service.ts
+++ RAS/client/src/app/services/students/students.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs/Observable";
 import 'rxjs/add/observable/of';
+import {StudentStatus} from "../../models/feedbacks/student.model";
 
 @Injectable()
 export class StudentsService{
@@ -12,12 +13,16 @@ export class StudentsService{
     return this.http.get('http://localhost:8080/students/585');
   }
 
-  addUsers(object: number[]):Observable<Object>{
+  getStatuses():Observable<any>{
+    return this.http.get('http://localhost:8080/students/statuses');
+  }
+
+  addUsers(object: number[]):Observable<any>{
     return this.http.post('http://localhost:8080/students/585/add', object);
   }
 
-  remove(academyId: number, studentId: number): Observable<any> {
-    let q = 'http://localhost:8080/students/{'+academyId+'}/{'+studentId+'}';
+  remove(studentId: number): Observable<any> {
+    let q = "http://localhost:8080/students/"+studentId;
     return this.http.delete(q);
   }
 }
