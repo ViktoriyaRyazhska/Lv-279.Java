27f5d2bc458b5ff1d465e3ddf6cf21a245450f29
shevtsov.bohdan <shevtsov.bohdan@gmail.com>
2017-12-14 12:45:55 +0200

    edited AcademyDTO

--- RAS/server/service/src/main/java/ua/softserve/service/dto/AcademyDTO.java
+++ RAS/server/service/src/main/java/ua/softserve/service/dto/AcademyDTO.java
@@ -11,34 +11,18 @@ import java.util.List;
 @Setter
 @ToString
 public class AcademyDTO {
-    // TODO: fix grammar errors
-    private Integer id;
-    private Integer academyId;
     private String grName;
     private String nameForSite;
     private int academyStagesId;
     private Long startDate;
     private Long endDate;
-    private int cityId;
     private int payment;
-    private int directionId;
-    private int technologieId;
-    private int profileId;
     private int studentPlannedToGraduate;
     private int studentPlannedToEnrollment;
     private int studentsActual;
-
     List<AcademyStages> academyStages;
     List<LanguageTranslations> cityNames;
     List<Directions> direction;
     List<Technologies> technologie;
-    List<ProfileInfo> profile; // TODO: rename field
-
-    private String paymentStatus;
-    private String profileName;
-    private String directionName;
-    private String technologyName;
-    private String cityName;
-    private String status;
-    private List<String> experts;
+    List<ProfileInfo> profile;
 }
