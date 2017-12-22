be962a2d16d4e3976b38398635bf47860d51b53a
orok7 <orestmykytyn@gmail.com>
2017-12-22 20:22:18 +0200

    configs for jenkins added

--- RAS/server/web/src/main/java/ua/softserve/config/WebInitializer.java
+++ RAS/server/web/src/main/java/ua/softserve/config/WebInitializer.java
@@ -1,25 +1,25 @@
- package ua.softserve.config;
+package ua.softserve.config;
 
- import org.springframework.boot.SpringApplication;
- import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
- import org.springframework.boot.autoconfigure.SpringBootApplication;
- import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
- import org.springframework.boot.builder.SpringApplicationBuilder;
- import org.springframework.boot.web.support.SpringBootServletInitializer;
- import org.springframework.context.annotation.ComponentScan;
- import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
- @Configuration
- @EnableAutoConfiguration
- @ComponentScan(value = "ua.softserve")
- @SpringBootApplication(scanBasePackages = "ua.softserve",exclude = {SecurityAutoConfiguration.class })
- public class WebInitializer extends SpringBootServletInitializer {
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-   return application.sources(WebInitializer.class);
-  }
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(value = "ua.softserve")
+@SpringBootApplication(scanBasePackages = "ua.softserve", exclude = {SecurityAutoConfiguration.class})
+public class WebInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebInitializer.class);
+    }
 
-  public static void main(String[] args) {
-   SpringApplication.run(WebInitializer.class, args);
-  }
- }
+    public static void main(String[] args) {
+        SpringApplication.run(WebInitializer.class, args);
+    }
+}
