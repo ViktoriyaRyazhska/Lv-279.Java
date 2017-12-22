bf75f50ca7e38b72290ec364a846472dee1a098e
Orest Melnyk <o.melnyk10@gmail.com>
2017-12-22 18:43:57 +0200

    update controller

--- RAS/server/web/src/main/java/ua/softserve/controller/AcademyController.java
+++ RAS/server/web/src/main/java/ua/softserve/controller/AcademyController.java
@@ -4,17 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ua.softserve.persistence.dto.GroupInformationDTO;
 import ua.softserve.persistence.entity.Academy;
-import ua.softserve.persistence.entity.GroupInfo;
-import ua.softserve.persistence.repo.GroupInfoRepository;
 import ua.softserve.service.AcademyService;
 import ua.softserve.service.GroupInfoService;
 import ua.softserve.service.dto.*;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 @RestController
 public class AcademyController {
@@ -24,24 +19,24 @@ public class AcademyController {
     @Autowired
     GroupInfoService groupInfoService;
 
-    @RequestMapping(value = "/academy/{academyId}", method = RequestMethod.GET, produces = { "application/json" })
-    public ResponseEntity<Academy> getAcademy(@PathVariable Integer academyId) {
-        return new ResponseEntity<>(academyService.findOne(academyId), HttpStatus.OK);
+    @GetMapping(value = "/group/{id}")
+    public ResponseEntity<Academy> getAcademy(@PathVariable Integer id) {
+        return new ResponseEntity<>(academyService.findOne(id), HttpStatus.OK);
     }
 
-    @RequestMapping(value = {"/academy/addgroup", "/getDropDownList"}, method = RequestMethod.GET, produces = { "application/json" })
+    @GetMapping(value = {"/group", "/getDropDownList"})
     public ResponseEntity<AcademyDropDownLists> getAllAcademies() {
         return new ResponseEntity<>(academyService.getAcademyDTO(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/academy/addgroup", method = RequestMethod.POST, produces = { "application/json" })
-    public void saveGroup(@RequestBody AcademyForSaveDTO academyDTO) {
-        academyService.saveAcademyFromAcademyDTO(academyDTO);
+    @PostMapping(value = "/group/add")
+    public ResponseEntity saveGroup(@RequestBody AcademyForSaveDTO academyDTO) {
+        academyService.saveAcademyAndGroupInfoFromAcademyDTO(academyDTO);
+        return ResponseEntity.ok().body(200);
     }
 
     @RequestMapping(value = "/viewAcademies", method = RequestMethod.GET, produces = { "application/json" })
     public ResponseEntity<List<AcademyForViewDTO>> searchSite() {
-        List<GroupInformationDTO> allInfo = groupInfoService.getAllInfo();
         return new ResponseEntity<>(groupInfoService.getAllAcademies(), HttpStatus.OK);
     }
 }
