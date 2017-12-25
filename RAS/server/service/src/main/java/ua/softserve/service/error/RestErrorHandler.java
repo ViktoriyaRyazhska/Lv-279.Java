81717c914a65fe8c1b849670d6a8873bf2b435d7
Orest Melnyk <o.melnyk10@gmail.com>
2017-12-25 12:01:08 +0200

    handle NoSuchElementException

--- RAS/server/service/src/main/java/ua/softserve/service/error/RestErrorHandler.java
+++ RAS/server/service/src/main/java/ua/softserve/service/error/RestErrorHandler.java
@@ -4,11 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ua.softserve.service.exception.InvalidDataException;
 import ua.softserve.service.exception.InvalidTimeFrameException;
 
+import java.util.NoSuchElementException;
+
 @ControllerAdvice
-public class RestErrorHandler {
+public class RestErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(InvalidDataException.class)
     public ResponseEntity<String> handleInvalidDataException(InvalidDataException invalidDataException) {
@@ -18,6 +21,11 @@ public class RestErrorHandler {
     @ExceptionHandler(InvalidTimeFrameException.class)
     public ResponseEntity<String> handleInvalidTimeFrameException(InvalidTimeFrameException invalidTimeFrameException) {
         return new ResponseEntity<String>(invalidTimeFrameException.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException noSuchElementException) {
+        return new ResponseEntity<String>(noSuchElementException.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
 }
